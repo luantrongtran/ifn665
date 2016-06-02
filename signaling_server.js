e04a0b78522af2277741ee6b1350906ee3084453
Luan T. Tran <luanlua12388@gmail.com>
2016-06-03 01:43:18 +1000

    make signaling server and http server run on same server

--- signaling_server.js
+++ signaling_server.js
@@ -2,6 +2,18 @@
  * Created by lua on 3/04/2016.
  */
 var WebSocketServer = require('ws').Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + "/"));
+
+var server = http.createServer(app)
+server.listen(port);
+
+console.log("http server listening on %d", port);
+
 var wss = new WebSocketServer({port: 8888});
 
 /**
